3bfb8efd8236aa1df6a92e9876c91f6647ae32f1
Savva <sav.van.ivan3@mail.ru>
2024-01-17 13:37:28 +0300

    HW 20

--- src/com/teachmeskillls/lesson20/hw/task1/Runner.java
+++ src/com/teachmeskillls/lesson20/hw/task1/Runner.java
@@ -1,6 +1,13 @@
 package com.teachmeskillls.lesson20.hw.task1;
 
 public class Runner {
+    /**
+     * 1. There is a service station. There can be a maximum of a certain number of cars in service per hundred.
+     * Create a class that will run in a separate thread and will add cars for service at the service station.
+     * Create a class that will run in a separate thread and will pick up fixed cars from the service station.
+     * Let the maximum number of machines available for service be specified in the interface for storing constants.
+     * Use synchronized, wait, notify.
+     */
     public static void main(String[] args) {
         STO sto = new STO();
         CarComing carInput = new CarComing(sto);
--- src/com/teachmeskillls/lesson20/hw/task2/Morning.java
+++ src/com/teachmeskillls/lesson20/hw/task2/Morning.java
@@ -1,6 +1,13 @@
 package com.teachmeskillls.lesson20.hw.task2;
 
 public class Morning {
+    /**
+     * 2. Create a program that simulates morning: reading the news, tomorrow, coffee.
+     * Each thread is given a name and priority from the config file.
+     * Let there be 3 threads.
+     * Create and run 3 threads.
+     * Make two options: using the Runnable interface and using the Thread class.
+     */
     public static void main(String[] args) {
         Breakfast breakfastM = new Breakfast();
         Coffee coffeeM = new Coffee();
