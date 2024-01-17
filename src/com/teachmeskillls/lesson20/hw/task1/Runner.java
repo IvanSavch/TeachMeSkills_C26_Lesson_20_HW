c3069258cffeb66e11d7930b69aaeac4c2cb50d0
Savva <sav.van.ivan3@mail.ru>
2024-01-17 13:33:37 +0300

    HW 20

--- src/com/teachmeskillls/lesson20/hw/task1/Runner.java
+++ src/com/teachmeskillls/lesson20/hw/task1/Runner.java
@@ -3,8 +3,8 @@ package com.teachmeskillls.lesson20.hw.task1;
 public class Runner {
     public static void main(String[] args) {
         STO sto = new STO();
-        CarInput carInput = new CarInput(sto);
-        CarOut carOut = new CarOut(sto);
+        CarComing carInput = new CarComing(sto);
+        CarLeave carOut = new CarLeave(sto);
         Thread input = new Thread(carInput);
         Thread output = new Thread(carOut);
         input.start();
